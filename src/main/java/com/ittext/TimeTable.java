bbe5b3a73ca7fd3e0a28856f2ead662037bd858f
Sabdavi <abdavi.saeed@gmail.com>
2021-12-01 13:47:58 +0100

    add TimeTable class for creating timetables

--- src/main/java/com/ittext/TimeTable.java
+++ src/main/java/com/ittext/TimeTable.java
@@ -1,2 +1,12 @@
-package com.ittext;public class TimeTable {
+package com.ittext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimeTable {
+
+    public List<Service> createTimeTable(List<Service> serviceList){
+        return new ArrayList<>();
+    }
+
 }
