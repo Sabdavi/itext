0b8774a0ff6a2cb1e49e30231a6c60e545b66205
Sabdavi <abdavi.saeed@gmail.com>
2021-12-01 13:57:13 +0100

    add test structure

---
+++ src/test/java/com/itext/TimeTableTest.java
@@ -0,0 +1,13 @@
+package com.itext;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TimeTableTest {
+
+    @Test
+    public void TestCreateTimeTable(){
+        assertTrue(1==1);
+    }
+}
