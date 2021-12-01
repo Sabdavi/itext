9d83925b81e1981c5696f945c54adf742ef729eb
Sabdavi <abdavi.saeed@gmail.com>
2021-12-01 13:45:58 +0100

    add Service class for modeling the service

--- src/main/java/com/ittext/Service.java
+++ src/main/java/com/ittext/Service.java
@@ -1,2 +1,34 @@
-package com.ittext;public class Service {
+package com.ittext;
+
+import java.util.Date;
+
+public class Service {
+
+    Company company;
+    Date departureTime;
+    Date arrivalTime;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Date departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Date getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Date arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
 }
